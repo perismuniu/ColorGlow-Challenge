db4f0f0b310c0b6e6c27a0a6e3af9a5890068552
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-12-06 13:06:55 +0300

    Pad component created that gives the buttons a color in Pad.jsx

--- App.jsx
+++ App.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import padsArray from "./pads"
+import Pad from "./Pad"
 
-export default function App() {
+export default function App(props) {
 
     const [padsList, setPadsList] = React.useState(padsArray)
 
     const buttonEl = padsList.map(pad => {
         return (
-            <button 
-                key={pad.id} 
-            >
-            </button>
+            <Pad 
+                key={pad.id}
+                color={pad.color}
+            />
         )
     })
     return (
